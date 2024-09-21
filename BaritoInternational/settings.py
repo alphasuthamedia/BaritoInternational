b4b2a1afe2d4d952f6e42219ef4cb9d149577b47
alpha.sutha <alphasuthamedia@gmail.com>
2024-09-21 20:14:30 +0700

    backup tugas 4

--- BaritoInternational/settings.py
+++ BaritoInternational/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p-hm^9p*ggtgjdh7%@mo+g=_cn-apn%y2@+h-gfe2+zw9bpcg7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
  
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "alpha-sutha-baritointernational.pbp.cs.ui.ac.id","http://alpha-sutha-baritointernational.pbp.cs.ui.ac.id/", "https://alpha-sutha-baritointernational.pbp.cs.ui.ac.id/"]
 
